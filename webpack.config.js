5a07b291836233f8c96b30b7bda661570bdbdbfb
Julia Belova <katishen@outlook.com>
2022-10-18 15:17:43 +0500

    files added

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
-    filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
-    library: 'ajs', // название нашей библиотеки
-    libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
-    libraryExport: 'default', // экспортируется имя default
-    globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
+    path: path.resolve(__dirname, "dist"), // каталог для результатов сборки
+    filename: "index.js", // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
+    library: "ajs", // название нашей библиотеки
+    libraryTarget: "umd", // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
+    libraryExport: "default", // экспортируется имя default
+    globalObject: "this", // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
   },
   module: {
     rules: [
@@ -16,7 +16,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
